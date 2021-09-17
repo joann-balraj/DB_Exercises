9fd20a38b12e52d1775d70ec34f017b9644f1ead
joann-balraj <joann.balraj@gmail.com>
2021-09-17 13:57:58 -0500

    Add files via upload

---
+++ tables_exercises.sql
@@ -0,0 +1,6 @@
+SHOW CREATE TABLE employees;
+Describe employees;
+--  Numeric type columns: "emp_no" and "
+-- String type columns: "first-name", "gender", and "last_name"
+-- Date type columns: "birth-date" and "hire_date"
+SHOW CREATE TABLE dept_manager;
